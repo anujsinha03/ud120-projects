abecaeb5bb6c34993fc40a4089aeebb4c718c83e
Anuj Sinha <sinhaanuj1996@gmail.com>
2018-05-20 03:47:08 +0530

    Added knn, random-forest and adaboost algo for our email training set

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -15,7 +15,7 @@ bumpy_fast = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 grade_slow = [features_train[ii][0] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if labels_train[ii]==1]
 
-
+"""
 #### initial visualization
 plt.xlim(0.0, 1.0)
 plt.ylim(0.0, 1.0)
@@ -26,19 +26,32 @@ plt.xlabel("bumpiness")
 plt.ylabel("grade")
 plt.show()
 ################################################################################
+"""
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.metrics import accuracy_score
+
+clf = [KNeighborsClassifier(4),
+       RandomForestClassifier(max_depth=10, n_estimators=10, min_samples_split = 70),
+       AdaBoostClassifier(learning_rate = 0.77)]
 
 
+acc = []
+for x in clf:
+    x.fit(features_train, labels_train)
+    pred = x.predict(features_test)
+    print round(accuracy_score(labels_test, pred), 3)
+
+"""
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
-
-
-
-
-
-
-
-
-try:
-    prettyPicture(clf, features_test, labels_test)
-except NameError:
-    pass
+i = 0
+for x in clf:
+    try:
+        plt.figure(i)
+        i = i+1
+        prettyPicture(x, features_test, labels_test)
+        plt.title(x)
+    except NameError:
+        pass
+"""
