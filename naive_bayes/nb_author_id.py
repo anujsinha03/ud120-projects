3ccbe79ae666a4bace6f83e0b3ffe3d569a40989
Anuj Sinha <sinhaanuj1996@gmail.com>
2018-05-20 03:38:38 +0530

    Added Naive Bayes and Accuracy code using sklearn

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python python -W ignore::DeprecationWarning
 
 """ 
     This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
@@ -14,6 +14,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -21,13 +23,15 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train,labels_train)
+print("Training time is:","%0.4f"%(time() - t0))
 
+t1 = time()
+pred = clf.predict(features_test)
+print("Prediction time is:","%0.4f"%(time() - t1))
 
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
+print("Accuracy:","%0.4f"%accuracy_score(labels_test, pred))
 
 
