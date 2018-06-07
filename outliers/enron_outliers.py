9c76d53d2234e042466082633c85fec7797c3362
Anuj Sinha <sinhaanuj1996@gmail.com>
2018-06-08 02:16:34 +0530

    Updated outliers file

--- outliers/enron_outliers.py
+++ outliers/enron_outliers.py
@@ -2,7 +2,7 @@
 
 import pickle
 import sys
-import matplotlib.pyplot
+import matplotlib.pyplot as plt
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
@@ -10,10 +10,27 @@ from feature_format import featureFormat, targetFeatureSplit
 ### read in data dictionary, convert to numpy array
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
 features = ["salary", "bonus"]
+data_dict.pop("TOTAL", 0)
 data = featureFormat(data_dict, features)
 
 
+sol = [(s,data_dict[s]['salary'],data_dict[s]['bonus'] ) for s in sorted(list(data_dict.keys())) if ((data_dict[s]['salary'] and data_dict[s]['bonus']) != "NaN")]
+def getKey(item):
+    return item[1], 
+print (sorted(sol, key=getKey, reverse = True)[:5])
+
+def getKey1(item):
+    return item[2]
+print ""
+print (sorted(sol, key=getKey1, reverse = True)[:5])
+
 ### your code below
+for point in data:
+    plt.scatter(point[0], point[1])
+plt.xlabel("salary")
+plt.ylabel("bonus")
+plt.show()
+
 
 
 
